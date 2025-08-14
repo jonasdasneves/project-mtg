91ff7e7cad6cf53bd0c5e268064926f5ca2f16f1
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 20:00:24 -0300

    first commit

---
+++ src/main/java/br/com/fiap/project_mgt/ProjectMgtApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.project_mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/project_mgt/ProjectMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.project_mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
